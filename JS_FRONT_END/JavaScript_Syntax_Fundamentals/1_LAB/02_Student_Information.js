a770acf2dbbc106874a5991de2e731a43a40e41d
TedoGeorgiev <headshoter882@gmail.com>
2024-07-02 09:27:18 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/01/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/01/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/02/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/02/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/03/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/03/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/04/images/image1.jpg
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/04/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/04/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/06/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/06/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/07/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/07/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/08/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/08/text.txt
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/09/images/image1.jpg
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/09/screenshot.png
    #	out/production/HTML_CSS/_3_Box_Model_And_Position/New_Exercises_2/09/text.txt

---
+++ JS_FRONT_END/JavaScript_Syntax_Fundamentals/1_LAB/02_Student_Information.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function solve (name, age, grade) {
+    let info = `Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`;
+    console.log(info);
+}
+
+solve('John', 15, 5.54678);
---
+++ JS_FRONT_END/JavaScript_Syntax_Fundamentals/1_LAB/Demo_3.js
@@ -0,0 +1,39 @@
+
+// Switch
+
+let color = 'red';
+
+switch (color) {
+
+    case 'red':
+        console.log('the color is red');
+        break;
+
+    case 'blue':
+        console.log('the sky is blue');
+        break;
+
+    case 'brown':
+        console.log('the bears are brown');
+        break;
+
+    default:
+        console.log('color is pink');
+}
+
+// logical operators
+
+console.log(!true); //false
+console.log(!!true); //true
+
+let a = 3;
+let b = 5;
+console.log(a > 5 && b < 9);
+
+console.log(typeof []);
+
+console.log(!'Tedi');
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+
+
